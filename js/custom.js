dd4003b298ad797ee96752cbb04ff83ae80fbf31
Bekir ŞENGÖNÜL <Bekir.Sengonul@gsb.gov.tr>
2025-04-18 16:44:25 +0300

    navbar düzeltme

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,21 @@
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  * File Description: Place here your custom scripts
  */
+
+$(document).ready(function() {
+    // Close mobile menu when clicking outside
+    $(document).on('click', function(event) {
+        var $target = $(event.target);
+        var $navbar = $('.navbar-collapse');
+        var $toggle = $('.navbar-toggle');
+        
+        // Check if the click is outside the navbar and toggle button
+        if (!$target.closest('.navbar-collapse').length && 
+            !$target.closest('.navbar-toggle').length && 
+            $navbar.hasClass('in')) {
+            $navbar.collapse('hide');
+        }
+
+       
+    });
+});
